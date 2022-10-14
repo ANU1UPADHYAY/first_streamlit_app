10fc39957c2fe1826733334e73904362ab78c116
ANU1UPADHYAY <114915520+ANU1UPADHYAY@users.noreply.github.com>
2022-10-14 15:44:21 +0530

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -32,9 +32,8 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 if not fruit_choice:
 streamlit.error("PLease select a fruit to get inof")
 else:
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
 
 except URLError as e:
