6e1f2024058bd891a87f291895b102f65c1ab4ab
ANU1UPADHYAY <114915520+ANU1UPADHYAY@users.noreply.github.com>
2022-10-14 15:40:55 +0530

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -28,9 +28,8 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 try:
 fruit_choice = streamlit.text_input('What fruit would you like information about?')
-#streamlit.write('The user entered ', fruit_choice)
 
-if not fruit_choice
+if not fruit_choice:
 streamlit.error("PLease select a fruit to get inof")
 else:
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
